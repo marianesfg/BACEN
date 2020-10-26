1763a1d5b8305b2181b13211a9a3ebfd925e9252
marianesfg <mariane.sfgodoy@gmail.com>
2020-10-25 23:54:26 -0300

    commit 1

--- BACEN/Controllers/PlanController.cs
+++ BACEN/Controllers/PlanController.cs
@@ -1,9 +1,6 @@
 ﻿using BACENBusiness;
 using BACENModel;
-using System.Collections;
 using System.Collections.Generic;
-using System.Data;
-using System.IO;
 using System.Web.Mvc;
 
 namespace BACEN.Controllers
